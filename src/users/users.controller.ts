1bb4900e09626b4b2f83bb2d82375b553b217df8
EderFornero <ederfornero.ef@gmail.com>
2024-01-12 17:06:12 -0300

    patch updated user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
+  @HttpCode(HttpStatus.OK)
   async findAll() {
     return [
       { id: 1, name: 'eder' },
@@ -21,5 +31,16 @@ export class UsersController {
     return {
       id,
     };
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async createUser(@Body() createNewUser: any) {
+    return createNewUser;
+  }
+
+  @Patch(':id')
+  async updateUser(@Body() updateNewUser: any, @Param('id') id: string) {
+    return { id, updateNewUser };
   }
 }
