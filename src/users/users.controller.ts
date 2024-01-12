6ba937ff287d702f1dc6f10c40c139fcaffc6b23
EderFornero <ederfornero.ef@gmail.com>
2024-01-12 16:44:22 -0300

    http status2

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  findAll() {
+  async findAll() {
     return [
       { id: 1, name: 'eder' },
       { id: 2, name: 'pepe' },
     ];
+  }
+
+  // @Get('ab*cd')
+  // async findAll() {
+  //   return 'This route uses a wildcard';
+  // }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.FOUND)
+  async findById(@Param('id') id: string) {
+    return {
+      id,
+    };
   }
 }
